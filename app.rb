fdd63e39be897834bdc063f2e6c9533fe8d7a505
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2023-12-20 08:06:08 +0000

    build erb files, hided apikey, build a fetch call,rendered needed data and image

--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+#require "json"
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
+  erb(:info_prompt)
+  end
+  
+  get("/results") do
+    @book_name = params.fetch("book_name")
+
+    secret_key = ENV.fetch("BOOK_SEARCH_ENGINE")
+    api_url = "https://www.googleapis.com/books/v1/volumes?q=#{@book_name}&maxResults=10&key=#{secret_key}"
+
+    raw_response = HTTP.get(api_url)
+  
+    raw_data_string = raw_response.to_s
+    
+    parsed_response = JSON.parse(raw_data_string)
+
+    first_item = parsed_response["items"].at(0)
+    volume_info = first_item.fetch("volumeInfo")
+    @book_title = volume_info.fetch("title")
+    @book_author = volume_info.fetch("authors")
+    @book_description = volume_info.fetch("description")
+    @page_count = volume_info.fetch("pageCount")
+    image_photo = volume_info.fetch("imageLinks")
+    @thumbnail = image_photo.fetch("thumbnail")
+
+    erb(:results)
+  end
+  
